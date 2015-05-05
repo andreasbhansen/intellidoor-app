ce30f1c91d775a3744449094654d24be72ecf947
andreasbhansen <andreasb.nor@gmail.com>
2015-05-05 22:37:56 +0200

    Material Design implemented

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,8 +14,9 @@ angular.module('starter.controllers', [])
 			$scope.listData = data.available_doors;
 		});
 
-		$scope.logout = function ()
+		$scope.signOut = function ()
 		{
+			localstorage.setObject('user', {});
 			$http({
 				method: 'GET',
 				url: 'http://10.0.0.106:3000/api/logout',
@@ -138,38 +139,38 @@ angular.module('starter.controllers', [])
 	})
 	.controller('LoginCtrl', function ($scope, $http, localstorage, $state)
 	{
-		/*if (angular.equals({}, localstorage.getObject('user')))
-		 {*/
-		$scope.signIn = function (user)
+		if (angular.equals({}, localstorage.getObject('user')))
 		{
-			$http({
-				method: 'POST',
-				url: 'http://10.0.0.106:3000/api/login/',
-				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-				transformRequest: function (obj)
-				{
-					var str = [];
-					for (var p in obj)
-					{
-						str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-					}
-					return str.join("&");
-				},
-				data: {password: user.password, user: user.username}
-			}).success(function (data)
+			$scope.signIn = function (user)
 			{
-				localstorage.set('username', user.username);
-				localstorage.setObject('user', data);
-				$state.go('home');
-			});
-		};
-		/*		}
-		 else
-		 {
-		 $state.go('home');
-		 }*/
+				$http({
+					method: 'POST',
+					url: 'http://10.0.0.106:3000/api/login/',
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+					transformRequest: function (obj)
+					{
+						var str = [];
+						for (var p in obj)
+						{
+							str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+						}
+						return str.join("&");
+					},
+					data: {password: user.password, user: user.username}
+				}).success(function (data)
+				{
+					localstorage.set('username', user.username);
+					localstorage.setObject('user', data);
+					$state.go('home');
+				});
+			};
+		}
+		else
+		{
+			$state.go('home');
+		}
 	})
-	.controller('DoorCtrl', function ($scope, $http, $stateParams, localstorage, $ionicLoading, $timeout)
+	.controller('DoorCtrl', function ($scope, $http, $stateParams, localstorage, $ionicLoading, $timeout, $ionicHistory, $mdToast)
 	{
 		var doorId = $stateParams._id;
 		var doors = localstorage.getObject('doors');
@@ -199,6 +200,29 @@ angular.module('starter.controllers', [])
 		$scope.integrationsInUse = $scope.door.user_info_for_door.integrations.length;
 
 		removeTakenIntegrationsFromAvailableIntegrationsList();
+
+
+		$scope.goBack = function ()
+		{
+			console.log('Goaback');
+			$ionicHistory.goBack();
+		};
+
+		$scope.toastPosition = {
+			bottom: false,
+			top: true,
+			left: false,
+			right: true
+		};
+		$scope.getToastPosition = function ()
+		{
+			return Object.keys($scope.toastPosition)
+				.filter(function (pos)
+				{
+					return $scope.toastPosition[pos];
+				})
+				.join(' ');
+		};
 
 		$scope.alterIntegration = function (integration)
 		{
@@ -238,6 +262,11 @@ angular.module('starter.controllers', [])
 
 		$scope.saveIntegration = function (i, integration)
 		{
+
+			$ionicLoading.show({
+				template: 'Saving...<br/><ion-spinner></ion-spinner>'
+			});
+
 			$http({
 				method: 'POST',
 				url: 'http://10.0.0.106:3000/api/update-integration-state-for-user-at-door',
@@ -246,22 +275,56 @@ angular.module('starter.controllers', [])
 			}).success(function (data)
 			{
 				console.log(data);
+
+				$timeout(function ()
+				{
+					$ionicLoading.hide();
+
+					var toast = $mdToast.simple()
+						.content('Integration saved!')
+						.action('OK')
+						.highlightAction(false)
+						.position($scope.getToastPosition());
+					$mdToast.show(toast).then(function ()
+					{
+
+					});
+				}, 2500);
+
 			}).error(function (err)
 			{
 				console.log(err);
 			});
 		};
 
-		$scope.pullToRefresh = function ()
+		$scope.refresh = function ()
 		{
+			$ionicLoading.show({
+				template: 'Refreshing...<br/><ion-spinner></ion-spinner>'
+			});
 			// To get a good screenshot of the refreshing
-			$timeout(function () {
-				getUpdatedDoorInformation();
+			$timeout(function ()
+			{
+				getUpdatedDoorInformation(function ()
+				{
+					$ionicLoading.hide();
+
+					var toast = $mdToast.simple()
+						.content('Refreshed list!')
+						.action('OK')
+						.highlightAction(false)
+						.position($scope.getToastPosition());
+					$mdToast.show(toast).then(function ()
+					{
+
+					});
+
+				});
 			}, 2500);
 		};
 
 
-		function getUpdatedDoorInformation()
+		function getUpdatedDoorInformation(callback)
 		{
 			var username = localstorage.get('username');
 
@@ -280,8 +343,7 @@ angular.module('starter.controllers', [])
 						$scope.door = door;
 					}
 				}
-				//Stop the ion-refresher from spinning
-				$scope.$broadcast('scroll.refreshComplete');
+				callback()
 
 			});
 		}
